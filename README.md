439d2bed7b9a6a89e9a2d270055be5321fd57f82
Cola <bcad1591@gmail.com>
2022-06-13 02:57:23 +0900

    Chore: 기본 설정

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# boilerplates
+# Issue Tracker
+
+## Base
+
+- TS + React + Emotion
+
+## Directory
+
+```directory
+.
+├─ 🗂︎config
+│   ├─ webpack.common.js
+│   ├─ webpack.dev.js
+│   └─ webpack.prod.js
+├─ 🗂︎env
+│   └─ .env || .env.dev + .env.prod
+├─ 🗂︎public
+│   └─ index.html
+└─ 🗂︎src
+    ├─ 🗂︎components
+    ├─ App.tsx
+    └─ index.tsx
+```
---
+++ config/webpack.dev.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+});
