53d212f9957e0cd45fab95193d05733ac99209ec
Cola <bcad1591@gmail.com>
2022-11-11 14:42:50 +0900

    in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return <div className="">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/a" element={<div>A</div>} />
+        <Route path="/b" element={<div>B</div>} />
+        <Route path="/c" element={<div>C</div>} />
+      </Routes>
+      <Link to="/">/ 페이지</Link>
+      <Link to="/a">/a 페이지</Link>
+      <Link to="/b">/b 페이지</Link>
+      <Link to="/c">/c 페이지</Link>
+    </div>
+  );
 };
 
 export default App;
